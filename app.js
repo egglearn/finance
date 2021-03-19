d1e3d42fcbeaf58cb9b4c0cb8d7cb3a2f4b6d5ee
marcus <marcushamlin1@gmail.com>
2021-03-19 21:09:56 +0100

    update

--- app.js
+++ app.js
@@ -15,6 +15,188 @@ const boxFour = document.querySelector(".four");
 const answerFour = document.querySelector(".answer-four");
 let clickedFour = false;
 
+
+var provider = new firebase.auth.GoogleAuthProvider();
+
+
+
+console.log(provider)
+const firebaseConfig = {
+  apiKey: "AIzaSyA1lDkpF-cthoatQgovVaxAgtSB2HwdouE",
+  authDomain: "practice-project-20e97.firebaseapp.com",
+  projectId: "practice-project-20e97",
+  storageBucket: "practice-project-20e97.appspot.com",
+  messagingSenderId: "457494582494",
+  appId: "1:457494582494:web:d8568fb27ab2c26df01e4f"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+
+
+function signInRedirect(provider) {
+  // [START auth_signin_redirect]
+  firebase.auth().signInWithRedirect(provider);
+  // [END auth_signin_redirect]
+}
+
+
+
+
+
+
+function callGoogleSignIn(){
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+       // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+       // The signed-in user info.
+       var user = result.user;
+       // ...
+ }).catch(function(error) {
+     // Handle Errors here.
+       var errorCode = error.code;
+       var errorMessage = error.message;
+       // The email of the user's account used.
+       var email = error.email;
+       // The firebase.auth.AuthCredential type that was used.
+       var credential = error.credential;
+    // ...
+ });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+firebase.auth()
+  .signInWithPopup(provider)
+  .then((result) => {
+    console.log("dlfkjslgjd;glksaj;glsj")
+    /** @type {firebase.auth.OAuthCredential} */
+    var credential = result.credential;
+
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+
+
+    // [START auth_google_signin_popup]
+    firebase.auth()
+      .signInWithPopup(provider)
+      .then((result) => {
+        /** @type {firebase.auth.OAuthCredential} */
+        var credential = result.credential;
+  
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+      }).catch((error) => {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+    // [END auth_google_signin_popup]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// firebase.initializeApp(firebaseConfig)
+// const txtEmail= document.getElementById("txtEmail")
+// const txtPassword= document.getElementById("txtPassword")
+const btnLogin= document.getElementById("btnLogin")
+// const btnSignup= document.getElementById("btnSignup")
+// const btnLogout= document.getElementById("btnLogout")
+
+btnLogin.addEventListener("click",function(){
+callGoogleSignIn()
+  });
+
+
+
+
+
+
+
+
+// signInRedirect()
+//  console.log("dsjeomog")
+// })
+// btnLogin.addEventListener("click", e=>{
+//   const email = txtEmail.value
+//   const pass = txtPassword.value
+//   const auth = firebase.auth()
+
+//   const promise = auth.signInWithEmailAndPassword(email,pass)
+// promise.catch(e=> console.log(e.message))
+
+// })
+
+
+
+
+function tester(event) {
+  console.log(document.getElementById("a").value);
+}
+tester();
+
 answerOne.addEventListener("click", function () {
   if (!clickedOne) {
     const displayAnswerOne = document.createElement("div");
@@ -52,4 +234,4 @@ answerFour.addEventListener("click", function () {
     boxFour.appendChild(displayAnswerFour);
     clickedFour = true;
   }
-});
+})
